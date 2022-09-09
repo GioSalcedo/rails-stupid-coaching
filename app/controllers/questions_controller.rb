29af9d544d59e09ce89f61cc44ffd948f2b44007
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-09-08 19:22:30 -0500

    answer ready 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
+    @ask == params
   end
 
-  def answer
+  def answer(ask = params)
+    if ask == "I am going to work right now!"
+      return "Great!"
+    elsif ask.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
   end
 end
