56b24681a0627f1832d8b68d3aea4ffdf77895c5
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-09-08 19:40:33 -0500

    logic done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
-    @ask == params
   end
 
-  def answer(ask = params)
-    if ask == "I am going to work right now!"
-      return "Great!"
-    elsif ask.include?("?")
-      return "Silly question, get dressed and go to work!"
+  def answer
+    @question = params["Ask"]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      return "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
